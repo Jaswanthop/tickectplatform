80ca1a5e5792e56a87b5cb57eede82d58c89b332
Jaswanthop <jaswanth09pasala@gmail.com>
2026-03-14 15:55:28 +0530

    Initial commit

---
+++ src/main/java/com/jaswanth/tickectplatform/TickectplatformApplication.java
@@ -0,0 +1,13 @@
+package com.jaswanth.tickectplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickectplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickectplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaswanth/tickectplatform/TickectplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaswanth.tickectplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickectplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
